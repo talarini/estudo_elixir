c13def887c776cd22d12bd742614eaee8128dc23
Thiago Conti <thiago.conti@locaweb.com.br>
2018-11-19 07:50:32 -0200

    updated to elixir 1.7.x

--- cards/mix.exs
+++ cards/mix.exs
@@ -5,7 +5,7 @@ defmodule Cards.MixProject do
     [
       app: :cards,
       version: "0.1.0",
-      elixir: "~> 1.6",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -23,7 +23,7 @@ defmodule Cards.MixProject do
     [ # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     
-      {:ex_doc, "~> 0.18.0"}
+      {:ex_doc, "~> 0.19.0"}
     ]
   end
 end
